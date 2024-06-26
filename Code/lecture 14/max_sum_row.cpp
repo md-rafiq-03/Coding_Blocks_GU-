014b4e84b884ebb1529e2861d13a80effbbc81c9
Md Rafeeq <mdrafiq22206@gmail.com>
2024-06-26 08:17:01 +0530

    lecture 14 uploaded

---
+++ Code/lecture 14/max_sum_row.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int  main(){
+    int n,m;
+    cin>>n>>m;
+    int arr[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    int sum=0;
+    int ind;
+    for(int i=0;i<n;i++){
+        int currsum=0;
+        for(int j=0;j<m;j++){
+            currsum+=arr[i][j];
+        }
+        if(currsum>sum){
+            ind =i;
+            sum=currsum;
+        }
+    }
+
+    cout<<ind<<endl;
+    return 0;
+
+}
